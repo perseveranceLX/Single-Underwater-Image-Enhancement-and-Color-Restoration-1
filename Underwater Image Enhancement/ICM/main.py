16eb8c1aed1f166f48df6217b7ca1d41c2eba916
mtyldz <umutyildiz1128@gmail.com>
2021-05-20 15:42:53 +0300

    CHANGE ICM -> main.py

--- Underwater Image Enhancement/ICM/main.py
+++ Underwater Image Enhancement/ICM/main.py
@@ -2,29 +2,28 @@ import os
 import numpy as np
 import cv2
 import natsort
-import xlwt
 from global_histogram_stretching import stretching
 from hsvStretching import HSVStretching
 from sceneRadiance import sceneRadianceRGB
-
 
 np.seterr(over='ignore')
 if __name__ == '__main__':
     pass
 
 # folder = "C:/Users/Administrator/Desktop/UnderwaterImageEnhancement/NonPhysical/ICM"
-folder = "C:/Users/Administrator/Desktop/Databases/Dataset"
+# folder = "C:/Users/Administrator/Desktop/Databases/Dataset"
+folder = "/home/mtyldz/Documents/python/Single-Underwater-Image-Enhancement-and-Color-Restoration"
 
 path = folder + "/InputImages"
 files = os.listdir(path)
-files =  natsort.natsorted(files)
+files = natsort.natsorted(files)
 
 for i in range(len(files)):
     file = files[i]
     filepath = path + "/" + file
     prefix = file.split('.')[0]
     if os.path.isfile(filepath):
-        print('********    file   ********',file)
+        print('********    file   ********', file)
         # img = cv2.imread('InputImages/' + file)
         img = cv2.imread(folder + '/InputImages/' + file)
         img = stretching(img)
@@ -32,4 +31,4 @@ for i in range(len(files)):
         # cv2.imwrite(folder + '/OutputImages/' + Number + 'Stretched.jpg', sceneRadiance)
         sceneRadiance = HSVStretching(sceneRadiance)
         sceneRadiance = sceneRadianceRGB(sceneRadiance)
-        cv2.imwrite('OutputImages/' + prefix + '_ICM.jpg', sceneRadiance)
+        cv2.imwrite(folder + '/OutputImages/' + prefix + '_ICM.jpg', sceneRadiance)
